d057d8b5a5a4e28489982bacd0c5ca21f354e159
victor_tungui <capishy@gmail.com>
2022-05-26 18:46:38 -0500

    Update gitignore and clear settings

--- MyPersonalToDoApp.Api/Startup.cs
+++ MyPersonalToDoApp.Api/Startup.cs
@@ -1,175 +1,175 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using Microsoft.EntityFrameworkCore;
-using MyPersonalToDoApp.Data;
-using Microsoft.AspNetCore.Mvc;
-using MyPersonalToDoApp.Data.Contracts;
-using MyPersonalToDoApp.Data.Repositories;
-using AutoMapper;
-using MyPersonalToDoApp.Api.Mapper;
-using Microsoft.AspNetCore.Identity;
-using MyPersonalToDoApp.DataModel.Identity;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.OpenApi.Models;
+//using Microsoft.EntityFrameworkCore;
+//using MyPersonalToDoApp.Data;
+//using Microsoft.AspNetCore.Mvc;
+//using MyPersonalToDoApp.Data.Contracts;
+//using MyPersonalToDoApp.Data.Repositories;
+//using AutoMapper;
+//using MyPersonalToDoApp.Api.Mapper;
+//using Microsoft.AspNetCore.Identity;
+//using MyPersonalToDoApp.DataModel.Identity;
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using Microsoft.IdentityModel.Tokens;
+//using System;
 
-namespace MyPersonalToDoApp.Api;
+//namespace MyPersonalToDoApp.Api;
 
-    //public class Startup
-    //{
-    //    public Startup(IConfiguration configuration)
-    //    {
-    //        Configuration = configuration;
-    //    }
+//public class Startup
+//{
+//    public Startup(IConfiguration configuration)
+//    {
+//        Configuration = configuration;
+//    }
 
-    //    public IConfiguration Configuration { get; }
+//    public IConfiguration Configuration { get; }
 
-    //    // This method gets called by the runtime. Use this method to add services to the container.
-    //    public void ConfigureServices(IServiceCollection services)
-    //    {
-    //        services.AddDbContext<ToDoContext>(options => 
-    //            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
-    //        );
-    //        services.AddCors(options =>
-    //        {
-    //            options.AddPolicy("AllowAllOrigins ", builder => builder.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed( o => true));
-    //        });
-    //        services.AddCors();
+//    // This method gets called by the runtime. Use this method to add services to the container.
+//    public void ConfigureServices(IServiceCollection services)
+//    {
+//        services.AddDbContext<ToDoContext>(options => 
+//            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+//        );
+//        services.AddCors(options =>
+//        {
+//            options.AddPolicy("AllowAllOrigins ", builder => builder.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed( o => true));
+//        });
+//        services.AddCors();
 
-    //        services.AddDatabaseDeveloperPageExceptionFilter();
-    //        services.AddControllers();
-    //        services.AddApiVersioning(setup => {
-    //            setup.DefaultApiVersion = new ApiVersion(1, 0);
-    //            setup.AssumeDefaultVersionWhenUnspecified = true;
-    //            setup.ReportApiVersions = true;
-    //        });
-    //        this.ConfigureSwagger(services);
-    //        this.ConfigureAuth(services);
-    //        this.ConfigureRepositories(services);
-    //        this.ConfigureMapper(services);
-    //    }
+//        services.AddDatabaseDeveloperPageExceptionFilter();
+//        services.AddControllers();
+//        services.AddApiVersioning(setup => {
+//            setup.DefaultApiVersion = new ApiVersion(1, 0);
+//            setup.AssumeDefaultVersionWhenUnspecified = true;
+//            setup.ReportApiVersions = true;
+//        });
+//        this.ConfigureSwagger(services);
+//        this.ConfigureAuth(services);
+//        this.ConfigureRepositories(services);
+//        this.ConfigureMapper(services);
+//    }
 
-    //    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    //    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    //    {
-    //        if (env.IsDevelopment())
-    //        {
-    //            app.UseDeveloperExceptionPage();
-    //            app.UseSwagger();
-    //            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyPersonalToDoApp.Api v1"));
-    //        }
+//    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//    {
+//        if (env.IsDevelopment())
+//        {
+//            app.UseDeveloperExceptionPage();
+//            app.UseSwagger();
+//            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyPersonalToDoApp.Api v1"));
+//        }
 
-    //        //app.UseHttpsRedirection();
+//        //app.UseHttpsRedirection();
 
-    //        //app.UseCors("AllowAllOrigins");
-    //        app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(o => true));
+//        //app.UseCors("AllowAllOrigins");
+//        app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(o => true));
 
-    //        app.UseRouting();            
-    //        app.UseAuthentication();
-    //        app.UseAuthorization();            
+//        app.UseRouting();            
+//        app.UseAuthentication();
+//        app.UseAuthorization();            
 
-    //        app.UseEndpoints(endpoints =>
-    //        {
-    //            endpoints.MapControllers();
-    //        });
-    //    }
+//        app.UseEndpoints(endpoints =>
+//        {
+//            endpoints.MapControllers();
+//        });
+//    }
 
-    //    #region helpers
+//    #region helpers
 
-    //    private void ConfigureSwagger(IServiceCollection services)
-    //    {
-    //        services.AddSwaggerGen(c =>
-    //        {
-    //            c.SwaggerDoc("v1", new OpenApiInfo { 
-    //                Title = "Personal Todo App", 
-    //                Version = "v1" });
+//    private void ConfigureSwagger(IServiceCollection services)
+//    {
+//        services.AddSwaggerGen(c =>
+//        {
+//            c.SwaggerDoc("v1", new OpenApiInfo { 
+//                Title = "Personal Todo App", 
+//                Version = "v1" });
 
-    //            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    //            {
-    //                Name = "Authorization",
-    //                Type = SecuritySchemeType.ApiKey,
-    //                Scheme = "Bearer",
-    //                BearerFormat = "JWT",
-    //                In = ParameterLocation.Header,
-    //                Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
-    //            });
+//            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+//            {
+//                Name = "Authorization",
+//                Type = SecuritySchemeType.ApiKey,
+//                Scheme = "Bearer",
+//                BearerFormat = "JWT",
+//                In = ParameterLocation.Header,
+//                Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
+//            });
 
-    //            c.AddSecurityRequirement(new OpenApiSecurityRequirement { 
-    //                {
-    //                    new OpenApiSecurityScheme {
-    //                        Reference = new OpenApiReference {
-    //                            Type = ReferenceType.SecurityScheme,
-    //                            Id = "Bearer"
-    //                        }
-    //                    },
-    //                    System.Array.Empty<string>()
-    //                }
-    //            });
-    //        });
-    //    }
+//            c.AddSecurityRequirement(new OpenApiSecurityRequirement { 
+//                {
+//                    new OpenApiSecurityScheme {
+//                        Reference = new OpenApiReference {
+//                            Type = ReferenceType.SecurityScheme,
+//                            Id = "Bearer"
+//                        }
+//                    },
+//                    System.Array.Empty<string>()
+//                }
+//            });
+//        });
+//    }
 
-    //    private void ConfigureAuth(IServiceCollection services)
-    //    {
-    //        services.AddIdentity<ApplicationUser, IdentityRole>()
-    //            .AddEntityFrameworkStores<ToDoContext>()
-    //            .AddDefaultTokenProviders();
+//    private void ConfigureAuth(IServiceCollection services)
+//    {
+//        services.AddIdentity<ApplicationUser, IdentityRole>()
+//            .AddEntityFrameworkStores<ToDoContext>()
+//            .AddDefaultTokenProviders();
 
-    //        services.AddAuthentication(options =>
-    //        {
-    //            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    //            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    //            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-    //        }).
-    //        AddJwtBearer(options => {
-    //            options.SaveToken = true;
-    //            options.RequireHttpsMetadata = false;
-    //            options.TokenValidationParameters = new TokenValidationParameters
-    //            {
-    //                ValidateIssuer = true,
-    //                ValidateAudience = true,
-    //                ValidAudience = Configuration["AuthJwt:ValidAudience"],
-    //                ValidIssuer = Configuration["AuthJwt:ValidIssuer"],
-    //                IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(Configuration["AuthJwt:Secret"])),
-    //                ClockSkew = TimeSpan.Zero
-    //            };
-    //        });
+//        services.AddAuthentication(options =>
+//        {
+//            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+//        }).
+//        AddJwtBearer(options => {
+//            options.SaveToken = true;
+//            options.RequireHttpsMetadata = false;
+//            options.TokenValidationParameters = new TokenValidationParameters
+//            {
+//                ValidateIssuer = true,
+//                ValidateAudience = true,
+//                ValidAudience = Configuration["AuthJwt:ValidAudience"],
+//                ValidIssuer = Configuration["AuthJwt:ValidIssuer"],
+//                IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(Configuration["AuthJwt:Secret"])),
+//                ClockSkew = TimeSpan.Zero
+//            };
+//        });
 
-    //        // Identity Configuration
-    //        services.Configure<IdentityOptions>(options => {
-    //            options.User.AllowedUserNameCharacters = Configuration.GetValue<string>("Identity:User:AllowedUserNameCharacters");
-    //            options.User.RequireUniqueEmail = Configuration.GetValue<bool>("Identity:User:RequireUniqueEmail");                
+//        // Identity Configuration
+//        services.Configure<IdentityOptions>(options => {
+//            options.User.AllowedUserNameCharacters = Configuration.GetValue<string>("Identity:User:AllowedUserNameCharacters");
+//            options.User.RequireUniqueEmail = Configuration.GetValue<bool>("Identity:User:RequireUniqueEmail");                
 
-    //            options.Password.RequireDigit = true;
-    //            options.Password.RequiredLength = 8;
-    //            options.Password.RequireUppercase = true;
-    //            options.Password.RequireNonAlphanumeric = true;                
+//            options.Password.RequireDigit = true;
+//            options.Password.RequiredLength = 8;
+//            options.Password.RequireUppercase = true;
+//            options.Password.RequireNonAlphanumeric = true;                
 
-    //            // 
-    //            options.Lockout.MaxFailedAccessAttempts = Configuration.GetValue<int>("Identity:Lockout:MaxFailedAccessAttempts");
-    //            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(Configuration.GetValue<int>("Identity:Lockout:DefaultLockoutTimeSpan"));
-    //        });
-    //    }
+//            // 
+//            options.Lockout.MaxFailedAccessAttempts = Configuration.GetValue<int>("Identity:Lockout:MaxFailedAccessAttempts");
+//            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(Configuration.GetValue<int>("Identity:Lockout:DefaultLockoutTimeSpan"));
+//        });
+//    }
 
-    //    private void ConfigureRepositories(IServiceCollection services)
-    //    {
-    //        services.AddScoped<IActivityRepository, ActivityRepository>();
-    //        services.AddScoped<IRegisterRepository, RegisterRepository>();
-    //        services.AddScoped<ICustomerRepository, CustomerRepository>();
-    //        services.AddScoped<ITodoRepository, TodoRepository>();
-    //    }
+//    private void ConfigureRepositories(IServiceCollection services)
+//    {
+//        services.AddScoped<IActivityRepository, ActivityRepository>();
+//        services.AddScoped<IRegisterRepository, RegisterRepository>();
+//        services.AddScoped<ICustomerRepository, CustomerRepository>();
+//        services.AddScoped<ITodoRepository, TodoRepository>();
+//    }
 
-    //    private void ConfigureMapper(IServiceCollection services)
-    //    {
-    //        // AutoMapper
-    //        var myTodoMapper = new MapperConfiguration(configure => {
-    //            configure.AddProfile(new MyPersonalToDoProfile());
-    //        });
-    //        services.AddSingleton(myTodoMapper.CreateMapper());
-    //    }
+//    private void ConfigureMapper(IServiceCollection services)
+//    {
+//        // AutoMapper
+//        var myTodoMapper = new MapperConfiguration(configure => {
+//            configure.AddProfile(new MyPersonalToDoProfile());
+//        });
+//        services.AddSingleton(myTodoMapper.CreateMapper());
+//    }
 
-    //    #endregion
-    //}
+//    #endregion
+//}
